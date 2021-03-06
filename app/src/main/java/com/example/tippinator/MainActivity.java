89d0a8532105bce16caec934d4eb1f57687fc3aa
Ty Williams <55842460+s532591@users.noreply.github.com>
2021-03-05 18:54:53 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/tippinator/MainActivity.java
+++ app/src/main/java/com/example/tippinator/MainActivity.java
@@ -5,10 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button avgContinent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        avgContinent = (Button) findViewById(R.id.avgContinent);
+        
+        buttonContinent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openMainActivityContinentSelector();}
+        });
     }
+    public void openMainActivityContinentSelector(){
+    }
+    
 }
